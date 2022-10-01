6dd55ae94742253395420bc977bd0c4d34daf9f3
keepgo-studio <keepgo.studio@gmail.com>
2022-10-01 12:16:55 +0900

    Complete structuring and styling

---
+++ js/font.js
@@ -0,0 +1,42 @@
+export class LeonSansFont {
+    _leon;
+    _ctx;
+    _sw;
+    _sh;
+    
+    pixelRatio = 2;
+
+    constructor(canvas, str, color, size, weight, width, height) {
+        this._ctx = canvas.getContext("2d");
+
+        this._sw = width;
+        this._sh = height;
+
+        canvas.width = this._sw * this.pixelRatio;
+        canvas.height = this._sh * this.pixelRatio;
+        canvas.style.width = this._sw + 'px';
+        canvas.style.height = this._sh + 'px';
+        this._ctx.scale(this.pixelRatio, this.pixelRatio);
+    
+        this._leon = new LeonSans({
+            text: str,
+            color,
+            size,
+            weight
+        });
+
+        requestAnimationFrame(this.animate.bind(this));
+    }
+
+    animate(t) {
+        requestAnimationFrame(this.animate.bind(this));
+    
+        this._ctx.clearRect(0, 0, this._sw, this._sh);
+    
+        const x = (this._sw - this._leon.rect.w) / 2;
+        const y = (this._sh - this._leon.rect.h) / 2;
+        this._leon.position(x, y);
+    
+        this._leon.draw(this._ctx);
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,27 @@
+import { LeonSansFont } from "./font.js";
+
+const logoText = "keepgo\nstudio";
+
+window.onload = () => {
+  // initializing for logo canvas
+  new LeonSansFont(
+    document.querySelector(".keepgo-studio-logo"),
+    logoText,
+    "#000",
+    80,
+    200,
+    300,
+    165
+  );
+  new LeonSansFont(
+    document.querySelector(".keepgo-studio-logo-s"),
+    logoText,
+    "#000",
+    20,
+    200,
+    70,
+    40
+  );
+
+	
+};

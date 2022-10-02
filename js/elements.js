de734db717aa1c300dad0bf4c010204ecb6e7179
keepgo-studio <keepgo.studio@gmail.com>
2022-10-02 18:28:27 +0900

    Refactoring for fade animations and fix issue for Navbar
     Navbar .current element occur error. Although the user has entered a
     specific section, navbar didn't update.

--- js/elements.js
+++ js/elements.js
@@ -1,7 +1,5 @@
 import { getSectionsHeight } from "./utils.js";
 
-const d = 10;
-
 export class Navbar {
   _element;
   _hideY = 0;
@@ -12,6 +10,8 @@ export class Navbar {
   currentElem;
   currentLeft = 0;
 
+  borderGap = 5;
+
   constructor() {
     this._element = document.getElementById("navbar");
     this.currentElem = document.querySelector("nav .current");
@@ -19,7 +19,7 @@ export class Navbar {
     this._hideY =
       document.querySelector(".header-text-container").offsetTop -
       this._element.offsetHeight;
-    this._transformY = document.getElementById("home").offsetHeight - d;
+    this._transformY = document.getElementById("home").offsetHeight - this.borderGap;
 
     this._sectionsOffsetTop = getSectionsHeight();
 
@@ -34,32 +34,32 @@ export class Navbar {
   init() {}
 
   animate() {
-    if (this._hideY < window.scrollY && window.scrollY < this._transformY) {
+    if (this._hideY <= window.scrollY && window.scrollY < this._transformY) {
       this._element.classList.add("hide");
     } else {
       this._element.classList.remove("hide");
     }
 
-    if (window.scrollY > this._transformY) {
+    if (window.scrollY >= this._transformY) {
       this._element.classList.add("nav2");
     } else {
       this._element.classList.remove("nav2");
     }
 
     this._sectionsOffsetTop.forEach((offsetTop, idx) => {
-      if (idx == 0 && window.scrollY < offsetTop - d) {
+      offsetTop -= this.borderGap;
+      if (idx == 0 && window.scrollY < offsetTop) {
         this.currentLeft = 0
       }
       else if (
         idx < this._sectionsOffsetTop.length &&
-        offsetTop < window.scrollY &&
-        window.scrollY < this._sectionsOffsetTop[idx + 1] - d
-      ) {
+        offsetTop <= window.scrollY &&
+        window.scrollY < this._sectionsOffsetTop[idx + 1]
+        ) {
         this.currentLeft = this._coor[idx];
-      } else if (offsetTop < window.scrollY - d) {
+      } else if (offsetTop <= window.scrollY) {
         this.currentLeft = this._coor[idx];
       }
-
     });
 
     if (this.currentLeft == 0) {
@@ -71,4 +71,8 @@ export class Navbar {
 
     requestAnimationFrame(this.animate.bind(this));
   }
+}
+
+class AboutSection {
+
 }
--- js/utils.js
+++ js/utils.js
@@ -38,58 +38,98 @@ export function getSectionsHeight() {
   return sectionsOffsetTop;
 }
 
+function _fadeSettingsStyles(
+  transitionFunction,
+  trasnitionDurationMs,
+  trasnitionDistance
+) {
+  return `
+    transition-timing-function: ${transitionFunction};
+    transition-duration: ${trasnitionDurationMs}ms;
+    top: ${trasnitionDistance}px;
+  `;
+}
+
 export class ElementsFadeIn {
+  distance = 10;
   _elements;
 
   constructor(elements) {
     this._elements = elements;
 
+    const fiStyle = document.createElement("style");
+    fiStyle.innerHTML = `
+    [fadeIn] {
+      ${_fadeSettingsStyles("ease", 500, this.distance)}
+    }
+    `
+
+    document.head.appendChild(fiStyle);
+
     this.attach();
   }
 
   attach() {
-    const io = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.style.opacity = 1;
-          entry.target.style.top = 0;
+    const io = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.top = 0;
 
-          observer.unobserve(entry.target)
-        }
-      })
-    }, {
-      threshold: 0.5
-    })
-    
-    this._elements.forEach(e => { io.observe(e) });
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
 
+    this._elements.forEach((e) => {
+      io.observe(e);
+    });
   }
 }
 export class ElementsFadeInOut {
+  distance = 10;
   _elements;
 
   constructor(elements) {
     this._elements = elements;
 
+    const fioStyle = document.createElement('style')
+    fioStyle.innerHTML = `
+    [fadeInOut]  {
+      ${_fadeSettingsStyles("ease-in-out", 300, this.distance)}
+    }
+    `
+
+    document.head.appendChild(fioStyle);
+
     this.attach();
   }
 
   attach() {
-    const io = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.style.opacity = 1;
-          entry.target.style.top = 0;
-        } else {
-          entry.target.style.opacity = 0;
-          entry.target.style.top = '10px';
-        }
-      })
-    }, {
-      threshold: 0.5
-    })
-    
-    this._elements.forEach(e => { io.observe(e) });
+    const io = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.top = 0;
+          } else {
+            entry.target.style.opacity = 0;
+            entry.target.style.top = `${this.distance}px`;
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
 
+    this._elements.forEach((e) => {
+      io.observe(e);
+    });
   }
 }
